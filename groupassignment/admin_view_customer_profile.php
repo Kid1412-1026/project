d012e25537d0db40d7881ea98ea5e5812572cc6c
Bernard Wong <banana1026w@gmail.com>
2025-01-12 14:01:49 +0800

    Update admin_view_customer_profile.php

--- groupassignment/admin_view_customer_profile.php
+++ groupassignment/admin_view_customer_profile.php
@@ -1,5 +1,5 @@
 <?php 
-    include("include/config.php");
+    include "include/config.php";
     session_start();
 ?>
 
@@ -16,77 +16,71 @@
 <body>
     <header>
         <div class="header">
-        <h1>Meeting Room Booking System</h1>  
+            <h1>Meeting Room Booking System</h1>  
         </div>
     </header>
 
     <nav class="topNav" id="topNav">
-        <?php
-            include("include/navMenu.php");
-        ?>
-            <a href="javascript:void(0);" class="icon" onClick="regulateNavMenu"><i class="fa fa-bars"></i></a>
+        <?php include "include/navMenu.php"; ?>
+        <a href="javascript:void(0);" class="icon" onClick="regulateNavMenu"><i class="fa fa-bars"></i></a>
     </nav>
 
     <main>
         <div class="pageTitle">
-            <br>
             <h1>Customer List</h1>
         </div>
 
-        <br><br>
-
         <?php 
-            $sql = "SELECT * FROM customer";
+            // Fetch customer data from the database
+            $sql = "SELECT cust_ID, cust_Name, cust_Phone_No FROM customer";
             $result = mysqli_query($conn, $sql);
-
-            $row = mysqli_fetch_assoc($result);
-            $cust_ID = $row["cust_ID"];
-            $cust_Name = $row["cust_Name"];
-            $cust_Phone_No = $row["cust_Phone_No"];
-
         ?>
 
-            <table border="1" id="adjustable">
-                <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>Customer ID</th>
-                        <th>Customer Name</th>
-                        <th>Customer Contact Number</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                        $sql = "SELECT * FROM customer";
-                        $result = mysqli_query($conn, $sql);
+        <table border="1" id="adjustable">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Customer ID</th>
+                    <th>Customer Name</th>
+                    <th>Customer Contact Number</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    if ($result && mysqli_num_rows($result) > 0) {
+                        $numRow = 1;
 
-                        if (mysqli_num_rows($result) > 0) {
-                            //Output data of each row
-                            $numrow = 1;
-                            while ($row = mysqli_fetch_assoc($result)) {
-                                echo "<tr>";
-                                echo "<td>" . $numrow . "</td><td>" . $row["cust_ID"] . "</td><td>" .
-                                $row["cust_Name"] . "</td><td>" . $row["cust_Phone_No"] . "</td>";
-                                echo '<td>';
-                                echo '<a href="admin_delete_customer_profile.php?id=' . 
-                                $row["cust_ID"] . '" onClick="return confirm(\'Delete?\')">Delete</a></td>';
-                                echo "</tr>" . "\n\t\t";
-                                $numrow++;
-                            }
-                        } else {
-                            echo '<tr><td colspan="6">0 results</td></tr>';
+                        // Loop through the result set
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr>";
+                            echo "<td>" . $numRow . "</td>";
+                            echo "<td>" . htmlspecialchars($row["cust_ID"]) . "</td>";
+                            echo "<td>" . htmlspecialchars($row["cust_Name"]) . "</td>";
+                            echo "<td>" . htmlspecialchars($row["cust_Phone_No"]) . "</td>";
+                            echo "<td>";
+                            echo '<a href="admin_delete_customer_profile.php?id=' . htmlspecialchars($row["cust_ID"]) . 
+                                '" onClick="return confirm(\'Are you sure you want to delete this record?\')">Delete</a>';
+                            echo "</td>";
+                            echo "</tr>";
+
+                            $numRow++;
                         }
-                        mysqli_close($conn);
-                    ?>
-                </tbody>
-            </table>
+                    } else {
+                        // Display a message if no records are found
+                        echo '<tr><td colspan="5">No customers found.</td></tr>';
+                    }
 
-        <br><br><br><br><br>
+                    // Free the result set and close the connection
+                    mysqli_free_result($result);
+                    mysqli_close($conn);
+                ?>
+            </tbody>
+        </table>
     </main>
 
     <footer>
-        <h3>@ KK34703 Web Engineering Group Assignment (Group 14) </h3>
+        <h3>&copy; KK34703 Web Engineering Group Assignment (Group 14)</h3>
     </footer>
 </body>
 
