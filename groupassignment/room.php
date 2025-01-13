ea075ed7763b07c8a2b42afa84fd193dc3ef56b4
dausaffendi <dausaffendi1999@gmail.com>
2025-01-13 20:07:52 +0800

    Update room.php

--- groupassignment/room.php
+++ groupassignment/room.php
@@ -13,16 +13,9 @@ session_start();
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <script>
         function updateRoomList() {
-            var roomTypeSelect = document.getElementById('roomType');
-            var roomTableBody = document.querySelector('#adjustable tbody');
-            var selectedRoomType = roomTypeSelect.value;
-
-            // Clear existing rows
+            const roomTableBody = document.querySelector('#adjustable tbody');
             roomTableBody.innerHTML = '';
         }
-
-        // Initial room list update
-        updateRoomList();
     </script>
 </head>
 
@@ -35,7 +28,7 @@ session_start();
 
     <nav class="topNav" id="topNav">
         <?php include("include/navMenu.php"); ?>
-        <a href="javascript:void(0);" class="icon" onClick="regulateNavMenu"><i class="fa fa-bars"></i></a>
+        <a href="javascript:void(0);" class="icon" onclick="regulateNavMenu"><i class="fa fa-bars"></i></a>
     </nav>
 
     <main>
@@ -45,74 +38,31 @@ session_start();
         <div class="roomList">
             <div class="option">
                 <form method="POST" id="roomFilter">
-                    <label for="">Room Type: &nbsp;</label>
-                    <select class="room_type_select" id="roomType" name="roomType" value=""
-                        onchange="this.form.submit()">
+                    <label for="roomType">Room Type: &nbsp;</label>
+                    <select class="room_type_select" id="roomType" name="roomType" onchange="this.form.submit()">
                         <?php
-                        $roomType = $_POST['roomType'];
-                        if ($roomType == null) {
-                            echo '<option value="All">All Room</option>';
-                            echo '<option value="Board Room">Board Room</option>';
-                            echo '<option value="Breakout Room">Breakout Room</option>';
-                            echo '<option value="Conference Room">Conference Room</option>';
-                            echo '<option value="Huddle Room">Huddle Room</option>';
-                            echo '<option value="Seminar Room">Seminar Room</option>';
-                            echo '<option value="Video Conference Room">Video Conference Room</option>';
-                        }
+                        $roomTypes = [
+                            "All" => "All Room",
+                            "Board Room" => "Board Room",
+                            "Breakout Room" => "Breakout Room",
+                            "Conference Room" => "Conference Room",
+                            "Huddle Room" => "Huddle Room",
+                            "Seminar Room" => "Seminar Room",
+                            "Video Conference Room" => "Video Conference Room"
+                        ];
+                        $selectedRoomType = $_POST['roomType'] ?? "All";
 
-                        if (isset($_POST['roomType'])) {
-                            $roomType = $_POST['roomType'];
-                            if ($roomType == "All") {
-                                echo '<option value="All" selected>All Room</option>';
-
-                            } else {
-                                echo '<option value="All">All Room</option>';
-                            }
-
-                            if ($roomType == "Board Room") {
-                                echo '<option value="Board Room" selected>Board Room</option>';
-
-                            } else {
-                                echo '<option value="Board Room">Board Room</option>';
-                            }
-
-                            if ($roomType == "Breakout Room") {
-                                echo '<option value="Breakout Room" selected>Breakout Room</option>';
-                            } else {
-                                echo '<option value="Breakout Room">Breakout Room</option>';
-                            }
-
-                            if ($roomType == "Conference Room") {
-                                echo '<option value="Conference Room" selected>Conference Room</option>';
-                            } else {
-                                echo '<option value="Conference Room">Conference Room</option>';
-                            }
-
-                            if ($roomType == "Huddle Room") {
-                                echo '<option value="Huddle Room" selected>Huddle Room</option>';
-                            } else {
-                                echo '<option value="Huddle Room">Huddle Room</option>';
-                            }
-
-                            if ($roomType == "Seminar Room") {
-                                echo '<option value="Seminar Room" selected>Seminar Room</option>';
-                            } else {
-                                echo '<option value="Seminar Room">Seminar Room</option>';
-                            }
-
-                            if ($roomType == "Video Conference Room") {
-                                echo '<option value="Video Conference Room" selected>Video Conference Room</option>';
-                            } else {
-                                echo '<option value="Video Conference Room">Video Conference Room</option>';
-                            }
+                        foreach ($roomTypes as $key => $label) {
+                            $selected = ($selectedRoomType === $key) ? "selected" : "";
+                            echo "<option value=\"$key\" $selected>$label</option>";
                         }
                         ?>
                     </select>
                 </form>
 
                 &nbsp;
-                <form method="submit" action="room_add.php" id="roomAdd" name="roomAdd">
-                    <input type="submit" id="add_button" name="add_button" value="Add New Room">
+                <form method="GET" action="room_add.php" id="roomAdd">
+                    <input type="submit" id="add_button" value="Add New Room">
                 </form>
             </div>
 
@@ -131,62 +81,37 @@ session_start();
                 </thead>
                 <tbody>
                     <?php
-                    if (isset($_POST['roomType'])) {
-                        $roomType = $_POST['roomType'];
+                    $roomTypeCondition = $selectedRoomType !== "All" ? "WHERE room_Type = '" . mysqli_real_escape_string($conn, $selectedRoomType) . "'" : "";
+                    $sql = "SELECT * FROM room $roomTypeCondition";
 
-                        // Fetch data based on selected room type
-                        if ($roomType == 'All') {
-                            $sql = "SELECT * FROM room";
-                        } else {
-                            $sql = "SELECT * FROM room WHERE room_type = '$roomType'";
+                    $result = mysqli_query($conn, $sql);
+                    if ($result && mysqli_num_rows($result) > 0) {
+                        $numrow = 1;
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $roomID = htmlspecialchars($row['room_ID']);
+                            $roomType = htmlspecialchars($row['room_Type']);
+                            $roomCapacity = htmlspecialchars($row['room_Capacity']);
+                            $roomPrice = htmlspecialchars($row['room_Price_Per_Hour']);
+                            $roomStatus = htmlspecialchars($row['room_Availability_Status']);
+
+                            echo "<tr>";
+                            echo "<td>$numrow</td>";
+                            echo "<td>$roomID</td>";
+                            echo "<td>$roomType</td>";
+                            echo "<td>$roomCapacity</td>";
+                            echo "<td>RM$roomPrice</td>";
+                            echo "<td>$roomStatus</td>";
+                            echo "<td>
+                                <a href=\"room_edit.php?id=$roomID\" class=\"btn-edit\">Edit</a>
+                                <a href=\"room_delete.php?id=$roomID\" class=\"btn-delete\" onclick=\"return confirm('Delete?')\">Delete</a>
+                                </td>";
+                            echo "</tr>";
+                            $numrow++;
                         }
-
-                        $result = mysqli_query($conn, $sql);
-
-                        if (mysqli_num_rows($result) > 0) {
-                            $numrow = 1;
-                            while ($row = mysqli_fetch_assoc($result)) {
-                                echo "<tr>";
-                                echo "<td>" . $numrow . "</td>";
-                                echo "<td>" . $row['room_ID'] . "</td>";
-                                echo "<td>" . $row['room_Type'] . "</td>";
-                                echo "<td>" . $row['room_Capacity'] . "</td>";
-                                echo "<td>RM" . $row['room_Price_Per_Hour'] . "</td>";
-                                echo "<td>" . $row['room_Availability_Status'] . "</td>";
-                                echo '<td> <a href="room_edit.php?id=' . $row["room_ID"] . '" style="background-color: #00ff00; padding: 5px 10px; text-decoration: none; border-radius: 5px; border: 1px solid #000000; color: black;">Edit</a>&nbsp;';
-                                echo '<a href="room_delete.php?id=' . $row["room_ID"] . '"" style="background-color: #ff0000; padding: 5px 10px; text-decoration: none; border-radius: 5px; border: 1px solid #000000; color: black; onClick="return confirm(\'Delete?\')">Delete</a></td>';
-                                echo "</tr>";
-                                $numrow++;
-                            }
-                        } else {
-                            echo "<tr><td colspan='7'>No results found</td></tr>";
-                        }
-                        mysqli_close($conn);
-
                     } else {
-                        $sql = "SELECT * FROM room";
-
-                        $result = mysqli_query($conn, $sql);
-
-                        if (mysqli_num_rows($result) > 0) {
-                            $numrow = 1;
-                            while ($row = mysqli_fetch_assoc($result)) {
-                                echo "<tr>";
-                                echo "<td>" . $numrow . "</td>";
-                                echo "<td>" . $row['room_ID'] . "</td>";
-                                echo "<td>" . $row['room_Type'] . "</td>";
-                                echo "<td>" . $row['room_Capacity'] . "</td>";
-                                echo "<td>RM" . $row['room_Price_Per_Hour'] . "</td>";
-                                echo "<td>" . $row['room_Availability_Status'] . "</td>";
-                                echo '<td> <a href="room_edit.php?id=' . $row["room_ID"] . '" style="background-color: #00ff00; padding: 5px 10px; text-decoration: none; border-radius: 5px; border: 1px solid #000000; color: black;">Edit</a>&nbsp;';
-                                echo '<a href="room_delete.php?id=' . $row["room_ID"] . '"" style="background-color: #ff0000; padding: 5px 10px; text-decoration: none; border-radius: 5px; border: 1px solid #000000; color: black; onClick="return confirm(\'Delete?\')">Delete</a></td>';
-                                echo "</tr>";
-                                $numrow++;
-                            }
-                        } else {
-                            echo "<tr><td colspan='7'>No results found</td></tr>";
-                        }
+                        echo "<tr><td colspan='7'>No results found</td></tr>";
                     }
+                    mysqli_close($conn);
                     ?>
                 </tbody>
             </table>
@@ -195,7 +120,7 @@ session_start();
     </main>
 
     <footer>
-        <h3>@ KK34703 Web Engineering Group Assignment (Group 14) </h3>
+        <h3>@ KK34703 Web Engineering Group Assignment (Group 14)</h3>
     </footer>
 </body>
 
